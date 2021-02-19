7d1553402a4a097be3d216f6e56b0e6e7f5ed469
Devolopment <webadmin@ubudevolpment>
2021-02-19 23:47:37 +0000

    checkdata

--- getdata.py
+++ getdata.py
@@ -3,8 +3,12 @@ import pandas as pd
 
 con = cx_Oracle.connect("QABOOL_USER/QABOOL_USER@10.101.1.11:1521/ORCL", encoding="utf-8")
 
-data = pd.read_sql('SELECT * FROM MCST.SIS_STUDENTS', con)
+data1 = pd.read_sql('SELECT * FROM MCST.SIS_STUDENTS', con)
+data2 = pd.read_sql('SELECT * FROM MCST.SIS_ADVISORY_LISTS', con)
 con.close()
 
-data.head() # take a peek at data
-data.to_excel('students2.xlsx')
+data1.head() # take a peek at data
+data1.to_excel('students.xlsx')
+
+data2.head()
+data2.to_excel('advisory.xlsx')
